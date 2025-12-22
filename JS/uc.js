6cbb17dcdfc5a33680d2199b7c1546c67bc83efd
ahmed gouda <ag021055@gmail.com>
2025-12-22 13:15:51 +0400

    designed the UC calc options and created functions regarding showing and hidding it

---
+++ JS/uc.js
@@ -0,0 +1,36 @@
+// show the Unit Converter section
+const uc = document.querySelector('.UC')
+const uc_options = document.querySelector('.uc-options')
+uc.addEventListener('click', function(){
+    uc_options.classList.toggle('show-uc-options')
+})
+
+// show name of the unit-converter when hover
+const options = [...document.querySelectorAll('.unit')]
+const tips = [...document.querySelectorAll('.tip')]
+options.forEach(ele =>{
+    ele.addEventListener('mouseover', ()=>{
+        let indexOfEle = options.indexOf(ele)
+        tips[indexOfEle].innerHTML = ele.dataset.title
+        tips[indexOfEle].style.display = 'block'
+    })
+    ele.addEventListener('mouseleave', ()=>{
+        let indexOfEle = options.indexOf(ele)
+        tips[indexOfEle].style.display = 'none'
+    })
+
+    // call show uc calc function on click
+    ele.addEventListener('click', show_uc_calculator)
+})
+
+// show unit converter calc
+const uc_calculator = document.querySelector('.uc-calculator')
+function show_uc_calculator(){
+    uc_calculator.classList.add('show-uc-calculator')
+    uc_options.classList.toggle('show-uc-options')
+}
+//  hide unit converter calc
+const hide_uc_btn = document.querySelector('.hide-uc-clac ion-icon')
+hide_uc_btn.addEventListener('click', ()=>{
+    uc_calculator.classList.remove('show-uc-calculator')
+})
