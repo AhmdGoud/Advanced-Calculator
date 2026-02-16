b5f1c737a7ac1421d2fd44e3794b754ad4124d78
ahmed gouda <ag021055@gmail.com>
2026-02-16 13:56:19 +0400

    enhanced the UI to be a better one

--- JS/script.js
+++ JS/script.js
@@ -1,173 +1,184 @@
 // pointer affect
-const pointer = document.querySelector('.pointer')
-setInterval(function(){
-    pointer.classList.toggle('pointerAffect')
-}, 500)
+const pointer = document.querySelector(".pointer");
+setInterval(function () {
+  pointer.classList.toggle("pointerAffect");
+}, 500);
 
 // the final result function
-const result = document.querySelector('.result')
-function finalResult(){
-    result.innerHTML = ''
-    for (let ele of arrayOfInputs){
-        result.innerHTML += ele
-    }
+let arrayOfInputs = [];
+const result = document.querySelector(".result");
+function finalResult() {
+  result.innerHTML = "";
+  for (let ele of arrayOfInputs) {
+    result.innerHTML += ele;
+  }
 }
 
-
 // accessing numbers and operands
-let arrayOfInputs = []
 let atleastOperand = false;
-const immediatresult = document.querySelector('.immediatresult');
-const inputs = document.querySelectorAll('.input .numbers-operans')
-inputs.forEach(function(input){
-    input.addEventListener('click', function(){
-        // checking if first or last input is an operator then adding operands to the result section
-        const isLastInputOperand = /[+/\-*%]/.test(arrayOfInputs.at(arrayOfInputs.length-1));
-        if (/[+/\-*%]/.test(input.innerHTML) && arrayOfInputs.length > 0 && isLastInputOperand
-        || /[+/\-*%]/.test(input.innerHTML) && arrayOfInputs.length === 0){
-            return;
-        }else{
-            arrayOfInputs.push(input.innerHTML);
-            finalResult()
-            togcolor()
-        }
+const immediatresult = document.querySelector(".immediatresult");
+const inputs = document.querySelectorAll(".input .numbers-operans");
+inputs.forEach(function (input) {
+  input.addEventListener("click", function () {
+    // checking if first or last input is an operator then adding operands to the result section
+    const isLastInputOperand = /[+/\-*%]/.test(
+      arrayOfInputs.at(arrayOfInputs.length - 1),
+    );
+    if (
+      (/[+/\-*%]/.test(input.innerHTML) &&
+        arrayOfInputs.length > 0 &&
+        isLastInputOperand) ||
+      (/[+/\-*%]/.test(input.innerHTML) && arrayOfInputs.length === 0)
+    ) {
+      return;
+    } else {
+      arrayOfInputs.push(input.innerHTML);
+      finalResult();
+      togcolor();
+    }
 
-        // the immediate  result
-        let lastOperand = arrayOfInputs.at(arrayOfInputs.length-1);
-        if (/[+/\-*%]/.test(input.innerHTML)){
-            atleastOperand = true;
-        }
-        if (atleastOperand && !/[+/\-*%]/.test(lastOperand)){
-            immediatresult.innerHTML = eval(result.innerHTML);
-        }
-    })
-})
-
+    // the immediate  result
+    let lastOperand = arrayOfInputs.at(arrayOfInputs.length - 1);
+    if (/[+/\-*%]/.test(input.innerHTML)) {
+      atleastOperand = true;
+    }
+    if (atleastOperand && !/[+/\-*%]/.test(lastOperand)) {
+      immediatresult.innerHTML = eval(result.innerHTML);
+    }
+  });
+});
 
 // clear result section
-const clear = document.getElementById('clear')
-clear.addEventListener('click', function(){
-    arrayOfInputs = []
-    result.innerHTML = ''
-    backspace.classList.remove('togcolor')
-    immediatresult.innerHTML = ''
-})
+const clear = document.getElementById("clear");
+clear.addEventListener("click", function () {
+  arrayOfInputs = [];
+  result.innerHTML = "";
+  backspace.classList.remove("togcolor");
+  immediatresult.innerHTML = "";
+});
 
 // equal feature
-const equal = document.getElementById('equal')
-equal.addEventListener('click', function(){
-    if (arrayOfInputs.length > '0' && !arrayOfInputs.at(arrayOfInputs.length-1).match(/[+/\-*]/)){
-        operationsForHistory()
-        result.innerHTML = eval(result.innerHTML)
-        arrayOfInputs = []
-        immediatresult.innerHTML = ''
-    }
-})
-
-
+const equal = document.getElementById("equal");
+equal.addEventListener("click", function () {
+  if (
+    arrayOfInputs.length > "0" &&
+    !arrayOfInputs.at(arrayOfInputs.length - 1).match(/[+/\-*]/)
+  ) {
+    operationsForHistory();
+    result.innerHTML = eval(result.innerHTML);
+    arrayOfInputs = [];
+    immediatresult.innerHTML = "";
+  }
+});
 
 // history operations
 const histroyOperations = [];
-const history = document.querySelector('.history')
-function operationsForHistory(){
-    // single operation in history
-    const singleOperation = {
-        Expresion: result.innerHTML,
-        theResult: eval(result.innerHTML)
-    }
-    if (histroyOperations.length > 9){
-        histroyOperations.shift()
-        histroyOperations.push(singleOperation);
-    }else{
-        histroyOperations.push(singleOperation);
-    }
+const history = document.querySelector(".history");
+function operationsForHistory() {
+  // single operation in history
+  const singleOperation = {
+    Expresion: result.innerHTML,
+    theResult: `= ${eval(result.innerHTML)}`,
+  };
+  if (histroyOperations.length > 9) {
+    histroyOperations.shift();
+    histroyOperations.push(singleOperation);
+  } else {
+    histroyOperations.push(singleOperation);
+  }
 
-    // storing at loacal storage
-    window.localStorage.setItem('operations', JSON.stringify(histroyOperations))
-    const operations = window.localStorage.getItem("operations")
-    const parsOperations = JSON.parse(operations)
-    history.innerHTML = ''
+  // storing at loacal storage
+  window.localStorage.setItem("operations", JSON.stringify(histroyOperations));
+  const operations = window.localStorage.getItem("operations");
+  const parsOperations = JSON.parse(operations);
+  history.innerHTML = "";
 
-    // iterate over the array of operations and append it to the page
-    for (let ele of parsOperations){
-        // creating elements
-        const theOperation = document.createElement('div')
-        theOperation.className = 'theoperation'
-        const theExpresion = document.createElement('div')
-        const theResultHistroy = document.createElement('div')
-        // assigning values and append
-        theExpresion.innerHTML = ele.Expresion;
-        theResultHistroy.innerHTML = ele.theResult
-        theOperation.append(theExpresion, theResultHistroy)
-        history.prepend(theOperation)
-    }
+  // iterate over the array of operations and append it to the page
+  for (let ele of parsOperations) {
+    // creating elements
+    const theOperation = document.createElement("div");
+    theOperation.className = "theoperation";
+    const theExpresion = document.createElement("div");
+    const theResultHistroy = document.createElement("div");
+    // assigning values and append
+    theExpresion.innerHTML = ele.Expresion;
+    theResultHistroy.innerHTML = ele.theResult;
+    theOperation.append(theExpresion, theResultHistroy);
+    history.prepend(theOperation);
+  }
 }
-
-
 
 // show history results and check if UC tap is open
 export let historyIsOpened = false;
-import {uc_optionsIsOpened} from './uc.js'
+import { uc_optionsIsOpened } from "./uc.js";
 
-const historyBtn = document.querySelector('.historybtn')
-const theHistory = document.querySelector('.history-section')
-historyBtn.addEventListener('click', function(){
-    if (!uc_optionsIsOpened){
-        theHistory.classList.toggle('show-histroy')
-        historyBtn.innerHTML === 'History' ? historyBtn.innerHTML = 'Keypad' : historyBtn.innerHTML = 'History'
-        historyIsOpened ? historyIsOpened = false : historyIsOpened = true;
-    }
-})
+const historyBtn = document.querySelector(".historybtn");
+const theHistory = document.querySelector(".history-section");
+historyBtn.addEventListener("click", function () {
+  if (!uc_optionsIsOpened) {
+    theHistory.classList.toggle("show-histroy");
+    historyBtn.innerHTML === "History"
+      ? (historyBtn.innerHTML = "Keypad")
+      : (historyBtn.innerHTML = "History");
+    historyIsOpened ? (historyIsOpened = false) : (historyIsOpened = true);
+  }
+});
 
 // clear history from screen and local storage
-const clearHistory = document.querySelector('.clearbtn')
-clearHistory.addEventListener('click', clearing)
-function clearing(){
-    window.localStorage.removeItem('operations')
-    history.innerHTML = '';
+const clearHistory = document.querySelector(".clearbtn");
+clearHistory.addEventListener("click", clearing);
+function clearing() {
+  window.localStorage.removeItem("operations");
+  history.innerHTML = "";
 }
 
 // the backspace removing method
-const backspace = document.querySelector('.backspace')
-backspace.addEventListener('click', function(){
-    arrayOfInputs.pop()
-    finalResult()
-    togcolor()
-    inputs.forEach(function(input){
-        let lastOperand = arrayOfInputs.at(arrayOfInputs.length-1);
-        if (input.innerHTML !== undefined && /[+/\-*%]/.test(input.innerHTML)){
-            atleastOperand = true;
-        }
-        if (atleastOperand && arrayOfInputs.length > 0 && !/[+/\-*%]/.test(lastOperand)){
-            immediatresult.innerHTML = eval(result.innerHTML);
-        }
-    })
-})
+const backspace = document.querySelector(".backspace");
+backspace.addEventListener("click", function () {
+  arrayOfInputs.pop();
+  finalResult();
+  togcolor();
+  inputs.forEach(function (input) {
+    let lastOperand = arrayOfInputs.at(arrayOfInputs.length - 1);
+    arrayOfInputs.length === 0 ? (immediatresult.innerHTML = "") : "";
+
+    if (input.innerHTML !== undefined && /[+/\-*%]/.test(input.innerHTML)) {
+      atleastOperand = true;
+    }
+    if (
+      atleastOperand &&
+      arrayOfInputs.length > 0 &&
+      !/[+/\-*%]/.test(lastOperand)
+    ) {
+      immediatresult.innerHTML = eval(result.innerHTML);
+    }
+  });
+});
 
 // change backspace color when result in empty
-function togcolor(){
-    if (arrayOfInputs.length >= 1){
-        backspace.classList.add('togcolor')
-    }else{
-        backspace.classList.remove('togcolor')
-    }
+function togcolor() {
+  if (arrayOfInputs.length >= 1) {
+    backspace.classList.add("togcolor");
+  } else {
+    backspace.classList.remove("togcolor");
+  }
 }
 
 // the parentheses method (still have some logic)
-const brackets = document.getElementById('brackets')
-brackets.addEventListener('click', function(){
-    if (!arrayOfInputs.includes('(') && !arrayOfInputs.includes(')')){
-        arrayOfInputs.push('(')
-    }else{
-        let indexOfBracket = arrayOfInputs.indexOf('(')
-        let last = arrayOfInputs.at(indexOfBracket+1)
-        let opened = arrayOfInputs.filter((ele) => ele === '(').length;
-        let closed = arrayOfInputs.filter((ele) => ele === ')').length;
-        if (opened > closed){
-            arrayOfInputs.push(')')
-        }else{
-            arrayOfInputs.push('(')
-        }
+const brackets = document.getElementById("brackets");
+brackets.addEventListener("click", function () {
+  if (!arrayOfInputs.includes("(") && !arrayOfInputs.includes(")")) {
+    arrayOfInputs.push("(");
+  } else {
+    let indexOfBracket = arrayOfInputs.indexOf("(");
+    let last = arrayOfInputs.at(indexOfBracket + 1);
+    let opened = arrayOfInputs.filter((ele) => ele === "(").length;
+    let closed = arrayOfInputs.filter((ele) => ele === ")").length;
+    if (opened > closed) {
+      arrayOfInputs.push(")");
+    } else {
+      arrayOfInputs.push("(");
     }
-    finalResult()
-})
+  }
+  finalResult();
+});
--- JS/uc.js
+++ JS/uc.js
@@ -1,83 +1,85 @@
 // show the Unit Converter section and check if history tap is open
-import {historyIsOpened} from './script.js'
+import { historyIsOpened } from "./script.js";
 export let uc_optionsIsOpened = false;
 
-const uc = document.querySelector('.UC')
-const uc_options = document.querySelector('.uc-options')
-uc.addEventListener('click', function(){
-    if (!historyIsOpened){
-        uc_options.classList.toggle('show-uc-options')
-        uc_optionsIsOpened ? uc_optionsIsOpened = false : uc_optionsIsOpened = true;
-    }
-})
+const uc = document.querySelector(".UC");
+const uc_options = document.querySelector(".uc-options");
+uc.addEventListener("click", function () {
+  if (!historyIsOpened) {
+    uc_options.classList.toggle("show-uc-options");
+    uc_optionsIsOpened
+      ? (uc_optionsIsOpened = false)
+      : (uc_optionsIsOpened = true);
+  }
+});
 
 // show name of the unit-converter when hover
-const options = [...document.querySelectorAll('.unit')]
-const tips = [...document.querySelectorAll('.tip')]
-const logo = document.querySelector('.logo')
-options.forEach(ele =>{
-    ele.addEventListener('mouseover', ()=>{
-        let indexOfEle = options.indexOf(ele)
-        tips[indexOfEle].innerHTML = ele.dataset.title
-        tips[indexOfEle].style.display = 'block'
-    })
-    ele.addEventListener('mouseleave', ()=>{
-        let indexOfEle = options.indexOf(ele)
-        tips[indexOfEle].style.display = 'none'
-    })
+const options = [...document.querySelectorAll(".unit")];
+const tips = [...document.querySelectorAll(".tip")];
+const logo = document.querySelector(".logo");
+options.forEach((ele) => {
+  ele.addEventListener("mouseover", () => {
+    let indexOfEle = options.indexOf(ele);
+    tips[indexOfEle].innerHTML = ele.dataset.title;
+    tips[indexOfEle].style.display = "block";
+  });
+  ele.addEventListener("mouseleave", () => {
+    let indexOfEle = options.indexOf(ele);
+    tips[indexOfEle].style.display = "none";
+  });
 
-    // call show uc calc function on click
-    ele.addEventListener('click', function(){
-        show_uc_calculator(ele)
-    })
-})
+  // call show uc calc function on click
+  ele.addEventListener("click", function () {
+    show_uc_calculator(ele);
+  });
+});
 
 // show unit converter calc
-const uc_calculator = document.querySelector('.uc-calculator')
-function show_uc_calculator(ele){
-    uc_calculator.classList.add('show-uc-calculator')
-    uc_options.classList.toggle('show-uc-options')
-    // we treated ele like the document to get its inner span
-    logo.innerHTML = ele.querySelector('span').innerHTML
+const uc_calculator = document.querySelector(".uc-calculator");
+function show_uc_calculator(ele) {
+  uc_calculator.classList.add("show-uc-calculator");
+  uc_options.classList.toggle("show-uc-options");
+  // we treated ele like the document to get its inner span
+  logo.innerHTML = ele.querySelector("span").innerHTML;
 }
 
 //  hide unit converter calc
-const hide_uc_btn = document.querySelector('.hide-uc-clac ion-icon')
-hide_uc_btn.addEventListener('click', ()=>{
-    uc_calculator.classList.remove('show-uc-calculator')
-})
+const hide_uc_btn = document.querySelector(".hide-uc-clac ion-icon");
+hide_uc_btn.addEventListener("click", () => {
+  uc_calculator.classList.remove("show-uc-calculator");
+});
 
 // update the input on click numbers
 let arraOfUnits = [];
-const howManyUnits = document.querySelector('.input-unit')
-const output = document.querySelector('.output-nuit')
-const allNums = document.querySelectorAll('.uc-inputs .numbers-operans')
-allNums.forEach(num =>{
-    num.addEventListener('click', function(){
-        arraOfUnits.push(num.innerHTML);
-        howManyUnits.innerHTML = ''
-        for (let unit of arraOfUnits){
-            howManyUnits.innerHTML += unit;
-        }
-        output.innerHTML = eval(howManyUnits.innerHTML * 1.6)
-    })
-})
+const howManyUnits = document.querySelector(".input-unit");
+const output = document.querySelector(".output-nuit");
+const allNums = document.querySelectorAll(".uc-inputs .numbers-operans");
+allNums.forEach((num) => {
+  num.addEventListener("click", function () {
+    arraOfUnits.push(num.innerHTML);
+    howManyUnits.innerHTML = "";
+    for (let unit of arraOfUnits) {
+      howManyUnits.innerHTML += unit;
+    }
+    output.innerHTML = eval(howManyUnits.innerHTML * 1.6);
+  });
+});
 
 // clear the uc-input
-const clearUC = document.querySelector('#clearUC')
-clearUC.addEventListener('click', function(){
-    howManyUnits.innerHTML = ''
-    output.innerHTML = ''
-    arraOfUnits = []
-})
+const clearUC = document.querySelector("#clearUC");
+clearUC.addEventListener("click", function () {
+  howManyUnits.innerHTML = "";
+  output.innerHTML = "";
+  arraOfUnits = [];
+});
 
 // uc backspace
-const uc_backspace = document.querySelector('.uc-backspace')
-uc_backspace.addEventListener('click', function(){
-    arraOfUnits.pop()
-    howManyUnits.innerHTML = ''
-    for (let unit of arraOfUnits){
-        howManyUnits.innerHTML += unit;
-    }
-    output.innerHTML = eval(howManyUnits.innerHTML * 1.6)
-})
+const uc_backspace = document.querySelector(".uc-backspace");
+uc_backspace.addEventListener("click", function () {
+  arraOfUnits.pop();
+  howManyUnits.innerHTML = "";
+  for (let unit of arraOfUnits) {
+    howManyUnits.innerHTML += unit;
+  }
+  output.innerHTML = eval(howManyUnits.innerHTML * 1.6);
+});
